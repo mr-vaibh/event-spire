163ca60820e3ef24093dc3c3ae72c9c42351ea47
Mr. VaiBH <mrvaibh0@gmail.com>
2020-04-23 16:08:03 +0530

    added location in models.py and index.html

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 class Centre(models.Model):
     centre_name = models.CharField(max_length=50, default='', blank=False)
+    centre_location = models.CharField(max_length=100, default='', blank=False)
     centre_contact = models.CharField(max_length=20, default='', blank=True)
     centre_address = models.TextField(max_length=300, default='', blank=True)
     centre_img = models.ImageField(upload_to='home/centre/', blank=True)
